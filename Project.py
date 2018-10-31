21316d1e6d2035c896d6d485db3c5047a9fa906c
Logan Becker <loganbe.18@gmail.com>
2018-10-30 21:57:34 -0400

    Added Comments

--- Project.py
+++ Project.py
@@ -9,9 +9,10 @@ Created on Sun Oct 28 18:04:31 2018
 import datetime
 import pickle
 
-#%%
-dateinfo = datetime.datetime.today()
+#%% Find Data and Time Info
+dateinfo = datetime.datetime.today() #Find Todays Date and Time
 
+#Disect the Information
 year = dateinfo.year
 month = dateinfo.month
 day = dateinfo.day
@@ -20,19 +21,20 @@ hour = dateinfo.hour
 minute = dateinfo.minute
 
 months = ['January','February.','March','April','May','June','July',
-          'Augest','Septemp','October','November','December']
+          'Augest','Septemp','October','November','December'] #Bank Of Months
 
-weekday_convert = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+weekday_convert = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'] #Bank of Days of Week
 
-
+#Convert 24hr clock to 12hr clock
 if hour > 12: 
     hour = hour-12
     suffixTime = 'pm'
 else:
     suffixTime = 'am'
-
+#Convert Number day of week to String
 DayOfWeek = weekday_convert[weekday]
 
+#Convert Month Number to Month Word
 monthword = months[month-1]
 if day == 1 or day == 21 or day == 31:
     suffix = 'st'
@@ -46,52 +48,52 @@ else:
 print(DayOfWeek + ', ' + monthword + str(day)+suffix)
 print(str(hour)+':'+str(minute),suffixTime)
 
-#%%
-Import_Info = open("Date.pickle","rb")
+#%% Import Old Data Info Save New Date Information
+Import_Info = open("Date.pickle","rb") #Import Old Date
 OldInfo = pickle.load(Import_Info)
 
-Upload_Info = {'Day':day,'WeekDay':DayOfWeek,'Month':monthword,'Year':year,'Hour':hour,'Min':minute}
+Upload_Info = {'Day':day,'WeekDay':DayOfWeek,'Month':monthword,'Year':year,'Hour':hour,'Min':minute} #Save New Date
 pickle_out = open("Date.pickle","wb")
 pickle.dump(Upload_Info,pickle_out)
 pickle_out.close()
 
-#%%
-Import_Info = open("Money.pickle","rb")
+#%% Import Old Info 
+Import_Info = open("Money.pickle","rb") #Import Old Money
 OldMoney = pickle.load(Import_Info)
 
-Import_Info = open("Left_Over.pickle","rb")
+Import_Info = open("Left_Over.pickle","rb") #Import Old LeftOvers
 OldLeftOver = pickle.load(Import_Info)
 LeftOvers = OldLeftOver['LeftOvers']
 
-Import_Info = open("Settings.pickle","rb")
+Import_Info = open("Settings.pickle","rb") #Import Old Settings
 Settings = pickle.load(Import_Info)
 Allowance = int(Settings['Allowance']);
 
-#%%
+#%% Check if New Day/Week/Month/Year
 if OldInfo['Day'] != day:
-    Daily = [0];
+    Daily = [0]; #If New Day; Reset
 else:
-    Daily = OldMoney['Daily']
+    Daily = OldMoney['Daily'] #Else Use Old Day Value
 
 if OldInfo['WeekDay'] != 'Sunday' and DayOfWeek == 'Sunday': #Last input was Not Sunday but today Is Sunday
-    if OldMoney['Weekly'] > 0:
-        LeftOverNew = OldMoney['Weekly']
+    if OldMoney['Weekly'] > 0: #If New Week and Money was Left Over
+        LeftOverNew = OldMoney['Weekly'] #Update LeftOvers
         LeftOvers = LeftOvers + LeftOverNew   
     Weekly = [Allowance]; #Reset Week
 else:
-    Weekly = OldMoney['Weekly']
+    Weekly = OldMoney['Weekly'] #Else Use Old Week Value
 
 if OldInfo['Month'] != monthword:
-    Monthly = [0];
+    Monthly = [0]; #If New Month; Reset
 else:
-    Monthly = OldMoney['Monthly']
+    Monthly = OldMoney['Monthly'] #Else Use Old Month Value
 
 if OldInfo['Year'] != year:
-    Yearly = [0];
+    Yearly = [0]; #If New Year; Reset
 else:
-    Yearly = OldMoney['Yearly']
+    Yearly = OldMoney['Yearly'] #Else Use Old Year Value
     
-#%% Save In Case Code Was Not Run
+#%% Save In Case No Changes Made
 Daily_Past = OldMoney['Daily']; Weekly_Past = OldMoney['Weekly']; 
 Monthly_Past = OldMoney['Monthly']; Yearly_Past = OldMoney['Yearly']
 
@@ -105,37 +107,38 @@ Left_Over = {'LeftOvers':LeftOvers}
 pickle_out = open("Left_Over.pickle","wb")
 pickle.dump(Left_Over,pickle_out)
 pickle_out.close()
-#%%
-exitflag = 1
+#%% Main Body
+exitflag = 1 #Use to Check if Exitied Code
 while exitflag == 1:
-    Which = input('Input, LeftOver, View, Settings or Exit? ')
-    if Which == 'Input' or Which == 'input':
+    Which = input('Input, LeftOver, View, Settings or Exit? ') #Main Options
+    if Which == 'Input' or Which == 'input': #If Chosen Input
         try:
-            Money = float(input('How Much Money? '))
+            Money = float(input('How Much Money? ')) 
             
-            if Money > 0:
+            if Money > 0: #If Spending Money
                 print('Spent $', Money)
-            else:
+            else: #If Refunding
                 print('Refund $', Money)
             
-            Daily.append(Money)
+            #Increase Daily/Monthly/Yearly
+            Daily.append(Money) 
             Monthly.append(Money)
             Yearly.append(Money)
             
-            if (sum(Weekly)-Money) < 0:
+            if (sum(Weekly)-Money) < 0: #If you spent over your allowance
                 print()
                 print('WARNING')
                 print('YOU HAVE GONE BELOW YOUR ALLOWANCE')
                 print()
     
-                if LeftOvers > 0:
+                if LeftOvers > 0: #If You have some leftovers and want to dip into it
                     print()
                     print('You Have $',LeftOvers,'In LeftOvers')
                     RollOver = input('Would You Like To Use Money From LeftOver? ')
-                    if RollOver == 'yes' or RollOver == 'Yes':
-                        LeftOvers = LeftOvers - Money
-                        if LeftOvers < 0:
-                            Weekly.append(LeftOvers)
+                    if RollOver == 'yes' or RollOver == 'Yes': #If you want to Take From LeftOvers
+                        LeftOvers = LeftOvers - Money #Take from LeftOvers
+                        if LeftOvers < 0: 
+                            Weekly.append(LeftOvers) 
                             LeftOvers = 0
                     else:
                         Weekly.append(-Money)
